ef0c2597dd37080bba0f15a99a88206c6372789e
Deepanshu2501 <tomarvashu445@gmail.com>
2026-08-07 13:28:26 +0530

    05-Homepage

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,15 +1,37 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import Image from 'next/image'
+import React from 'react'
+import ProjectForm from '../modules/home/components/project-form'
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <Button>Add</Button>
-      <UserButton />
+    <div className='flex items-center justify-center w-full px-4 py-8'>
+      <div className='max-w-5xl w-full'>
+        <section className='space-y-8 flex flex-col items-center'>
+          <div className='flex flex-col items-center'>
+            <Image 
+              src={'/logo.svg'}
+              height={100}
+              width={100}
+              alt='logo'
+              className='hidden md:block invert dark:invert-0'
+            />
+          </div>
+          <h1 className='text-2xl md:text-5xl font-bold text-center'>Build Something with 💕</h1>
+
+          <p className='text-lg md:text-xl text-muted-foreground text-center'>
+            Create apps and websites by chatting with AI
+          </p>
+
+          <div className='max-w-3xl w-full'>
+            <ProjectForm />
+          </div>
+        </section>
+      </div>
     </div>
-  );
+  )
 }
+
+export default HomePage
 
 
 
@@ -68,5 +90,15 @@ Step : wrap children inside theme provider in layout file
 
 Step : Create another file for model toggle button inside components and use that btn with signed or signup btn in navbar.jsx
 
+Chapter : Building Homepage Ui
 
-*/
+Step : Format home page or root page.jsx file
+
+Step : Create project form component which is used on homepage
+npm i react-textarea-autosize
+npm i zod
+npm i react-hoo-form
+npm i @hookform/resolvers
+
+
+*/ 
